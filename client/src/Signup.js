57af58f227ad4be05d485080351cea340ff2b00e
Unknown <Calvin.lapp@dcmail.ca>
2021-08-01 22:23:58 -0400

    picture kinda good?

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -30,171 +30,49 @@ const Signup = (props) => {
   }
 
   return (
-    <MainForm
-      buttonText="Create"
-      onSubmit={handleRegister}
-      titleText={`Create an account.`}
-    >
-      <FormField
-        label="Username"
-        ariaLabel="username"
-        type="text"
-        name="username"
-      />
-      <FormField
-        label="E-mail address"
-        ariaLabel="e-mail address"
-        type="email"
-        name="email"
-      />
-      <FormField
-        label="Password"
-        ariaLabel="password"
-        type="password"
-        name="password"
-        inputProps={{ minlength: 6 }}
-        error={!!formErrorMessage.confirmPassword}
+    <>
+      <MainForm
+        buttonText="Create"
+        onSubmit={handleRegister}
+        titleText={`Create an account.`}
+        alternateButtonText={`Login`}
+        alternateText={`Already have an account?`}
+        alternateButtonOnClick={() => history.push("/login")}
       >
-        <FormHelperText>{formErrorMessage.confirmPassword}</FormHelperText>
-      </FormField>
-      <FormField
-        label="Confirm Password"
-        ariaLabel="confirm password"
-        type="password"
-        name="confirmPassword"
-        inputProps={{ minlength: 6 }}
-        error={!!formErrorMessage.confirmPassword}
-      >
-        <FormHelperText>{formErrorMessage.confirmPassword}</FormHelperText>
-      </FormField>
-    </MainForm>
-    // <Grid container>
-    //   <Grid item xs={5} className={`${styles.imageBox}`} />
-    //   <Grid item xs={7}>
-    //     <Grid container justifyContent="center" alignItems="center">
-    //       {/* Already Have an Account */}
-    //       <Grid container justifyContent="flex-end" spacing={4} style={{padding: 3 * 8}}>
-    //         <Grid
-    //           container
-    //           item
-    //           xs={7}
-    //           md={9}
-    //           justify="flex-end"
-    //           alignItems="center"
-    //         >
-    //           <Typography color="secondary" align="center" variant="inherit">
-    //             Already have an account?
-    //           </Typography>
-    //         </Grid>
-    //         <Grid item xs={5} md={3}>
-    //           <Grid container>
-    //             <Button
-    //               onClick={() => history.push("/login")}
-    //               color="primary"
-    //               size="large"
-    //               className={`${styles.boxShadow} ${styles.buttonPadding}`}
-    //             >
-    //               Login
-    //             </Button>
-    //           </Grid>
-    //         </Grid>
-    //       </Grid>
-    //       {/* Sign up Form */}
-    //       <Box p={10} width={1/2}>
-    //         <Grid container spacing={3}>
-    //           <Grid item xs={12}>
-    //             <Typography color="black" variant="h5">
-    //               Create an account.
-    //             </Typography>
-    //           </Grid>
-    //           <Grid item xs={12}>
-    //             <form onSubmit={handleRegister}>
-    //               <Grid container spacing={3}>
-    //                 <Grid item xs={12}>
-    //                   <FormControl fullWidth={true}>
-    //                     <FormLabel>Username</FormLabel>
-    //                     <TextField
-    //                       aria-label="username"
-    //                       name="username"
-    //                       type="text"
-    //                       fullWidth={true}
-    //                       margin="dense"
-    //                       required
-    //                     />
-    //                   </FormControl>
-    //                 </Grid>
-    //                 <Grid item xs={12}>
-    //                   <FormControl fullWidth={true}>
-    //                     <FormLabel>E-mail address</FormLabel>
-    //                     <TextField
-    //                       aria-label="e-mail address"
-    //                       type="email"
-    //                       name="email"
-    //                       required
-    //                     />
-    //                   </FormControl>
-    //                 </Grid>
-    //                 <Grid item xs={12}>
-    //                   <FormControl
-    //                     fullWidth={true}
-    //                     item
-    //                     error={!!formErrorMessage.confirmPassword}
-    //                   >
-    //                     <FormLabel>Password</FormLabel>
-    //                     <TextField
-    //                       aria-label="password"
-    //                       type="password"
-    //                       inputProps={{ minLength: 6 }}
-    //                       name="password"
-    //                       required
-    //                     />
-    //                     <FormHelperText>
-    //                       {formErrorMessage.confirmPassword}
-    //                     </FormHelperText>
-    //                   </FormControl>
-    //                 </Grid>
-    //                 <Grid item xs={12}>
-    //                   <div>
-    //                     <FormControl
-    //                       fullWidth={true}
-    //                       item
-    //                       error={!!formErrorMessage.confirmPassword}
-    //                     >
-    //                       <FormLabel>Confirm Password</FormLabel>
-    //                       <TextField
-    //                         aria-label="confirm password"
-    //                         type="password"
-    //                         inputProps={{ minLength: 6 }}
-    //                         name="confirmPassword"
-    //                         required
-    //                       />
-    //                       <FormHelperText>
-    //                         {formErrorMessage.confirmPassword}
-    //                       </FormHelperText>
-    //                     </FormControl>
-    //                   </div>
-    //                 </Grid>
-    //                 <Grid item xs={12}>
-    //                   <Grid container justifyContent="center">
-    //                     <Button
-    //                       type="submit"
-    //                       variant="contained"
-    //                       size="large"
-    //                       color="primary"
-    //                       className={`${styles.buttonPadding}`}
-    //                     >
-    //                       Create
-    //                     </Button>
-    //                   </Grid>
-    //                 </Grid>
-    //               </Grid>
-    //             </form>
-    //           </Grid>
-    //         </Grid>
-    //       </Box>
-    //     </Grid>
-    //   </Grid>
-    // </Grid>
+        <FormField
+          label="Username"
+          ariaLabel="username"
+          type="text"
+          name="username"
+        />
+        <FormField
+          label="E-mail address"
+          ariaLabel="e-mail address"
+          type="email"
+          name="email"
+        />
+        <FormField
+          label="Password"
+          ariaLabel="password"
+          type="password"
+          name="password"
+          inputProps={{ minLength: 6 }}
+          error={!!formErrorMessage.confirmPassword}
+        >
+          <FormHelperText>{formErrorMessage.confirmPassword}</FormHelperText>
+        </FormField>
+        <FormField
+          label="Confirm Password"
+          ariaLabel="confirm password"
+          type="password"
+          name="confirmPassword"
+          inputProps={{ minLength: 6 }}
+          error={!!formErrorMessage.confirmPassword}
+        >
+          <FormHelperText>{formErrorMessage.confirmPassword}</FormHelperText>
+        </FormField>
+      </MainForm>
+    </>
   );
 };
 
--- client/src/components/UserForm/MainForm.tsx
+++ client/src/components/UserForm/MainForm.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { Grid, Typography, Button, Theme } from "@material-ui/core";
+import { Grid, Typography, Button, Theme, Box } from "@material-ui/core";
 import { withStyles, createStyles } from "@material-ui/core/styles";
+import formImage from '../../assets/images/formImage.png'
 
 interface Props {
   titleText: string;
   buttonText: string;
+  alternateButtonText: string;
+  alternateButtonOnClick: () => void;
+  alternateText: string;
   onSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
   classes: {
     btnStyles: string;
+    btnBoxShadow: string;
   };
 }
 
@@ -15,6 +20,9 @@ const styles = ({ spacing }: Theme) =>
   createStyles({
     btnStyles: {
       padding: `${spacing(2)}px ${spacing(6)}px`,
+    },
+    btnBoxShadow: {
+      boxShadow: "0 0 6px 3px #e5e5e5",
     },
   });
 
@@ -24,34 +32,67 @@ const MainForm: React.FC<Props> = ({
   titleText,
   children,
   classes,
+  alternateButtonOnClick,
+  alternateButtonText,
+  alternateText,
 }) => {
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography color="initial" variant="h5">
-            {titleText}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <form onSubmit={onSubmit}>
-            <Grid container spacing={3}>
-              {children}
-            </Grid>
-            <Grid container justifyContent="center">
-              <Button
-                type="submit"
-                variant="contained"
-                size="large"
-                color="primary"
-                className={classes.btnStyles}
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid item xs={4} style={{backgroundImage: `url(${formImage})`, backgroundSize: 'cover'}}/>
+          <Grid item xs={8}>
+            <Grid container justifyContent="flex-end" spacing={3} style={{marginTop: 30}}>
+              <Grid
+                container
+                item
+                xs={5}
+                alignItems="center"
+                justifyContent="flex-end"
               >
-                {buttonText}
-              </Button>
+                <Typography color="secondary" align="center" variant="inherit">
+                  {alternateText}
+                </Typography>
+              </Grid>
+              <Grid item xs={5} sm={4} md={3}>
+                <Button
+                  onClick={alternateButtonOnClick}
+                  color="primary"
+                  size="large"
+                  className={`${classes.btnStyles} ${classes.btnBoxShadow}`}
+                >
+                  {alternateButtonText}
+                </Button>
+              </Grid>
             </Grid>
-          </form>
+            <Grid container spacing={2} justifyContent="center" style={{marginBottom: 30}}>
+              <Grid item xs={10}>
+                <Typography color="initial" variant="h5">
+                  {titleText}
+                </Typography>
+              </Grid>
+              <Grid item xs={10}>
+                <form onSubmit={onSubmit}>
+                  <Grid container spacing={3}>
+                    {children}
+                    <Grid item container justifyContent="center">
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        size="large"
+                        color="primary"
+                        className={classes.btnStyles}
+                      >
+                        {buttonText}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </form>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 };
