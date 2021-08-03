bc054d21d94f17a886a943d69a4e19ad0b2a6c55
Unknown <Calvin.lapp@dcmail.ca>
2021-08-03 14:24:22 -0400

    added a check for a user being in a conversation
    I believe the bug was here where you could send a different conversation Id and because you only check if the request has an Id you could change said Id and send that flag that as a message that is apart of that conversation.  So we check if a conversation between these two exists before creating that message

--- client/src/components/ActiveChat/Input.js
+++ client/src/components/ActiveChat/Input.js
@@ -37,8 +37,7 @@ class Input extends Component {
       text: event.target.text.value,
       recipientId: this.props.otherUser.id,
       conversationId: this.props.conversationId,
-      sender: this.props.user,
-      // sender: this.props.conversationId ? null : this.props.user,
+      sender: this.props.user
     };
     
     await this.props.postMessage(reqBody);
--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -11,12 +11,7 @@ router.post("/", async (req, res, next) => {
     }
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
-
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
-    }
+    
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
       senderId,
@@ -32,7 +27,12 @@ router.post("/", async (req, res, next) => {
       if (onlineUsers.includes(sender.id)) {
         sender.online = true;
       }
+    } else {
+      // conversation exists already, send the message
+      const message = await Message.create({ senderId, text, conversationId });
+      return res.json({ message, sender });
     }
+    
     const message = await Message.create({
       senderId,
       text,
