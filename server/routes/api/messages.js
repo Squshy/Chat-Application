5db75fc40636f4c66f45417459d8780a75b896de
Unknown <Calvin.lapp@dcmail.ca>
2021-08-03 18:33:44 -0400

    Checking for conversation before sending a message
    Before, if there was a conversationId in the request then you just add the message to that conversation but if the conversationId is altered on the way to the server then you could add the message to any conversation.  If we check if the conversation exists between the 2 users first then send the message it should be okay

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -11,11 +11,6 @@ router.post("/", async (req, res, next) => {
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
-    }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
       senderId,
@@ -31,6 +26,10 @@ router.post("/", async (req, res, next) => {
       if (onlineUsers.includes(sender.id)) {
         sender.online = true;
       }
+    } else {
+      // Already is a conversation between the users
+      const message = await Message.create({ senderId, text, conversationId });
+      return res.json({ message, sender });
     }
     const message = await Message.create({
       senderId,
