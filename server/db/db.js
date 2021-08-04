8078b1280c4052b7271e1799ac716015298f7d96
Unknown <Calvin.lapp@dcmail.ca>
2021-08-04 12:07:10 -0400

    using .env for db user

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres@localhost:5432/messenger", {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.DB_USER}@localhost:5432/messenger`, {
   logging: false
 });
 
